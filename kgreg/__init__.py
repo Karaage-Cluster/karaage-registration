fc1a602cd81f8b2344135772fc5f7910614fbbe8
Sam Morrison <sam@vpac.org>
2011-04-06 12:03:32 +1000

    New version 1.0.4

--- kgreg/__init__.py
+++ kgreg/__init__.py
@@ -16,7 +16,7 @@
 # along with Karaage  If not, see <http://www.gnu.org/licenses/>.
 
 
-VERSION = (1, 0, 3, 'final', 0)
+VERSION = (1, 0, 4, 'final', 0)
 
 def get_version():
     """ Return the current version"""
