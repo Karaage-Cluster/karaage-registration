8b7bb2f1275e8e7ed979464551f04804e38e0fb0
Sam Morrison <sam@vpac.org>
2012-02-14 10:27:22 +1100

    Added password reset templates

--- kgreg/conf/urls.py
+++ kgreg/conf/urls.py
@@ -20,6 +20,7 @@ urlpatterns += patterns('karaage.people.views.user',
     url(r'^profile/edit/$', 'edit_profile', name='kg_profile_edit'),
     url(r'^change_password/$', 'password_change', name='kg_user_change_password'),
     url(r'^change_password/done/$', 'password_change_done', name='kg_user_password_done'),
+    url(r'^accounts/password_reset/$', 'password_reset', name='password_reset'),
 
 )
 
@@ -39,9 +40,9 @@ urlpatterns += patterns('',
     url(r'^usage/', include('karaage.usage.urls.user')),
     url(r'^ajax_selects/', include('ajax_select.urls')),
     url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc',),
+    url(r'^pbs/', include('django_pbs.servers.urls')),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    url(r'^admin/', include(admin.site.urls)),  
-
+    url(r'^admin/', include(admin.site.urls)),
 )
 from karaage.applications.saml import SAMLInstituteForm
 from karaage.people.models import Institute
@@ -56,11 +57,13 @@ urlpatterns += patterns('karaage.backports',
     url(r'^accounts/login/$', 'login', {'extra_context': login_extra }, name='login'),
 
 )
-
+from karaage.people.forms import SetPasswordForm
 urlpatterns += patterns('django.contrib.auth.views',
 #    url(r'^accounts/login/$', 'login', {'extra_context': login_extra }, name='login'),
     url(r'^accounts/logout/$', 'logout', name='logout'),
-
+    url(r'^accounts/password_reset/done/$', 'password_reset_done', name='password_reset_done'),
+    url(r'^accounts/reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', 'password_reset_confirm', {'set_password_form': SetPasswordForm}, name='password_reset_confirm'),
+    url(r'^accounts/reset/done/$', 'password_reset_complete', name='password_reset_complete'),
 )
 
 
