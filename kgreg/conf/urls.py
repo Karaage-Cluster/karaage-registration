f099404db023381fa8de3b9fbd74dd6b38974145
Brian May <brian@microcomaustralia.com.au>
2014-02-12 12:10:54 +1100

    More changes for latest karaage.

--- kgreg/conf/urls.py
+++ kgreg/conf/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import *
 from django.conf import settings
-from django.contrib import admin
-
 
 urlpatterns = patterns('',
 )
@@ -11,27 +9,7 @@ try:
 except IOError:
     pass
 
-
-urlpatterns += patterns('',
-    url(r'^$', 'karaage.legacy.simple.direct_to_template', {'template': 'index.html'}, name='index'),
-    url(r'^aup/$', 'karaage.legacy.simple.direct_to_template', {'template': 'aup.html'}, name="aup"),
-    url(r'^persons/', include('karaage.people.urls.persons')),
-    url(r'^profile/', include('karaage.people.urls.profile')),
-    url(r'^groups/', include('karaage.people.urls.groups')),
-    url(r'^institutes/', include('karaage.institutes.urls')),
-    url(r'^projects/', include('karaage.projects.urls')),
-    url(r'^machines/', include('karaage.machines.urls')),
-    url(r'^software/', include('karaage.software.urls')),
-    url(r'^applications/', include('karaage.applications.urls')),
-    url(r'^captcha/', include('captcha.urls')),
-    url(r'^usage/', include('karaage.usage.urls')),
-    url(r'^ajax_selects/', include('ajax_select.urls')),
-    url(r'^xmlrpc/$', 'django_xmlrpc.views.handle_xmlrpc',),
-)
-
-if settings.DEBUG:
-    urlpatterns += patterns('',
-        (r'^karaage_graphs/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.GRAPH_ROOT}),
-    )
+import karaage.conf.urls
+urlpatterns += karaage.conf.urls.urlpatterns
 
 execfile("/etc/karaage/registration_urls.py")
