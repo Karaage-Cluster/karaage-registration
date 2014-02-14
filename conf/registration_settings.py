d5f934992fa95b3f129d2c69d3eaf9ad18f6565f
Brian May <brian@microcomaustralia.com.au>
2014-02-14 14:41:13 +1100

    Encourage setting conf/global_settings.py instead.

--- conf/registration_settings.py
+++ conf/registration_settings.py
@@ -1,10 +1,4 @@
 # Locally defined Karaage-registration settings
-# Place local settings in here.
-# Will override default settings
 
-#AUP_URL = "/users/aup/"
-
-#LOGIN_REDIRECT_URL = '/users/profile/'
-
-# Allow anyone to request an account
-#ALLOW_REGISTRATIONS = False
+# It is preferable to use /etc/karaage/global_settings.py if possible, unless
+# the setting would otherwise break karaage-admin.
