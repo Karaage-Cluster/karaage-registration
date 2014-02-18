6e21945e6871e36cf811180edd7c67c561a2c144
Brian May <brian@microcomaustralia.com.au>
2014-02-19 10:26:17 +1100

    Remove template now in Karaage.

--- kgreg/conf/settings.py
+++ kgreg/conf/settings.py
@@ -1,18 +1,12 @@
 # Django settings for kgreg project.
 from karaage.conf.settings import *
 
-TEMPLATE_DIRS += (
-    "/usr/share/kgreg/templates",
-)
-
 ROOT_URLCONF = 'kgreg.conf.urls'
 
 SITE_ID = 2
 
 STATIC_ROOT = '/var/lib/karaage-registration/static'
 STATIC_URL = '/kgreg_media/'
-
-TEMPLATE_CONTEXT_PROCESSORS += ('karaage.common.context_processors.registration',)
 
 ADMIN_IGNORED = True
 
