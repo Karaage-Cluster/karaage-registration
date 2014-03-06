cb603773b4b20536607d1c71e695215202ded32f
Brian May <brian@microcomaustralia.com.au>
2014-03-06 16:25:45 +1100

    Move version string.

--- setup.py
+++ setup.py
@@ -60,27 +60,8 @@ for dirpath, dirnames, filenames in os.walk(code_dir):
     elif filenames:
         data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
 
-path = "kgreg/__init__.py"
-with open(path, 'rU') as file:
-    t = compile(file.read(), path, 'exec', ast.PyCF_ONLY_AST)
-    for node in (n for n in t.body if isinstance(n, ast.Assign)):
-        if len(node.targets) == 1:
-            name = node.targets[0]
-            if isinstance(name, ast.Name) and \
-                    name.id in ('__version__', '__version_info__', 'VERSION'):
-                v = node.value
-                if isinstance(v, ast.Str):
-                    version = v.s
-                    break
-                if isinstance(v, ast.Tuple):
-                    r = []
-                    for e in v.elts:
-                        if isinstance(e, ast.Str):
-                            r.append(e.s)
-                        elif isinstance(e, ast.Num):
-                            r.append(str(e.n))
-                    version = '.'.join(r)
-                    break
+with open('VERSION.txt', 'r') as f:
+    version = f.readline().strip()
 
 media_files = []
 for dirpath, dirnames, filenames in os.walk('media'):
